db86457482f4dbde84e64bc64916fc462885b862
NathanKell <nathankell@gmail.com>
2014-04-23 21:39:31 -0400

    Improve launch clamp refueling: does all parts.

--- Source/RefuelingPump.cs
+++ Source/RefuelingPump.cs
@@ -12,7 +12,7 @@ namespace RealFuels
 	{
 
 		[KSPField(isPersistant = true)]
-		double pump_rate = 100.0; // 625 liters/second seems reasonable.
+		double pump_rate = 100.0;
 
 		public override string GetInfo ()
 		{
@@ -25,9 +25,8 @@ namespace RealFuels
             {
 
             }
-            else if (timestamp > 0 && part.parent != null && part.parent.Modules.Contains("ModuleFuelTanks"))
+            else if (timestamp > 0 && part.parent != null && part.vessel != null)
             {
-                // We're connected to a fuel tank, so let's top off any depleting resources
                 FillAttachedTanks(precisionDeltaTime);
             }
             base.OnUpdate();            //Needs to be at the end to prevent weird things from happening during startup and to make handling persistance easy; this does mean that the effects are delayed by a frame, but since they're constant, that shouldn't matter here
@@ -37,19 +36,24 @@ namespace RealFuels
         private void FillAttachedTanks(double deltaTime)
         {
             // now, let's look at what we're connected to.
-            ModuleFuelTanks m = (ModuleFuelTanks)part.parent.Modules["ModuleFuelTanks"];
-
-            // look through all tanks inside this part
-            foreach (ModuleFuelTanks.FuelTank tank in m.fuelList)
+            foreach (Part p in vessel.Parts) // look through all parts
             {
-                // if a tank isn't full, start filling it.
-                if (tank.amount < tank.maxAmount)
+                if (p.Modules.Contains("ModuleFuelTanks"))
                 {
-                    double top_off = deltaTime * pump_rate;
-                    if (tank.amount + top_off < tank.maxAmount)
-                        tank.amount += top_off;
-                    else
-                        tank.amount = tank.maxAmount;
+                    ModuleFuelTanks m = (ModuleFuelTanks)part.parent.Modules["ModuleFuelTanks"];
+                    // look through all tanks inside this part
+                    foreach (ModuleFuelTanks.FuelTank tank in m.fuelList)
+                    {
+                        // if a tank isn't full, start filling it.
+                        if (tank.amount < tank.maxAmount && tank.fillable)
+                        {
+                            double top_off = deltaTime * pump_rate;
+                            if (tank.amount + top_off < tank.maxAmount)
+                                tank.amount += top_off;
+                            else
+                                tank.amount = tank.maxAmount;
+                        }
+                    }
                 }
             }
         }
