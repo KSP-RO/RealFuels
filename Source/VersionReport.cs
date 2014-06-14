b41172e5e10fcdf20cbdae2dc7a75c043b6f87bd
Bill Currie <bill@taniwha.org>
2014-06-15 08:59:34 +0900

    Report the version of MFT/RF in the log file.
    While talking about displaying the version in-game in irc, I realized that
    there might be times when something goes wrong with the GUI and the version
    can't be determined. Putting the version in the log file as soon as the mod
    is loaded should take care of most cases.

---
+++ Source/VersionReport.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using KSPAPIExtensions;
+
+using KSP.IO;
+
+namespace RealFuels {
+
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	public class ExSettingsCreatorSpawn : MonoBehaviour
+	{
+
+		void Start ()
+		{
+			Debug.Log (MFSSettings.GetVersion ());
+			Destroy (this);
+		}
+	}
+}
