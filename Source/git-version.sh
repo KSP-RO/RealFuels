93bb4c318be249cd9426cdb309410e261b3e5d15
NathanKell <nathankell@gmail.com>
2014-08-12 02:19:58 -0400

    Fix csproj issue, update way version stored

--- Source/git-version.sh
+++ Source/git-version.sh
@@ -1,9 +1,10 @@
 #! /bin/sh
 
 full_version=`./git-version-gen --prefix rf .tarball-version s/rf-v/rf/`
-version=`echo $full_version | sed -e 's/-/\t/' | cut -f 1`
+file_version=`echo $full_version | sed -e 's/-/\t/' | cut -f 1`
+version=`echo $file_version | sed 's/\./\t/g' | cut -f1-2 | sed -e 's/\t/\./g'`
 
-sed -e "s/@FULL_VERSION@/$full_version/" -e "s/@VERSION@/$version/" AssemblyInfoRF.in > AssemblyInfoRF.cs-
+sed -e "s/@FULL_VERSION@/$full_version/" -e "s/@FILE_VERSION@/$file_version/" -e "s/@VERSION@/$version/" AssemblyInfoRF.in > AssemblyInfoRF.cs-
 cmp -s AssemblyInfoRF.cs AssemblyInfoRF.cs- || mv AssemblyInfoRF.cs- AssemblyInfoRF.cs
 
 full_version=`./git-version-gen --prefix mft .tarball-version s/mft-/mft/`
