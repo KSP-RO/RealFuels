1dd5ebb447c0226761516d9f4bd8fabfd9c4dbcf
Starwaster <starwaster@gmail.com>
2014-09-29 19:36:10 -0400

    Update HeatPump.cs
    - Fixed GUI issues for good
    - Changed part cooling to configurable node system. (select which attachment nodes in addition to the parent part which will be cooled)
    - Absolutely no Comic Sans.
    
    Point #2 above: added HEATPUMP_NODE to ModuleHeatPump config. 
    Example: The pump below will look for an attachment node named latch and will cool that part down.
    It also will not conduct any heat at all and has its dissipation increased to 1.0. Finally, heatGain will actually transfer the heat from the part (tank/engine whatever) into the fin, which will NOT try to move it back to the cooled part because it will not conduct. *whew*
    	MODULE
    	{
    		name = ModuleHeatPump
    		heatTransfer = 78
    		heatDissipation = 1.0
    		heatGain = 1.0
    		heatConductivity = 0.0
    		
    		RESOURCE
    		{
    			name = ElectricCharge
    			rate = 0.114
    		}      
    
    		HEATPUMP_NODE
    		{
    			name = latch
    		}
    	}

--- Source/HeatPump.cs
+++ Source/HeatPump.cs
@@ -14,8 +14,10 @@ namespace RealFuels
         {
             public string name;
             public float rate;
-            public int id {
-                get {
+            public int id
+            {
+                get 
+                {
                     return name.GetHashCode ();
                 }
             }
@@ -52,16 +54,20 @@ namespace RealFuels
         public void Activate ()
         {
             isActive = true;
-            base.Events ["Shutdown"].active = true;
-            base.Events ["Activate"].active = false;
+            Events ["Shutdown"].active = true;
+            Events ["Activate"].active = false;
+            //Events ["Shutdown"].guiActive = true;
+            //Events ["Activate"].guiActive = false;
         }
 
-        [KSPEvent(guiName = "Shutdown Heat Pump", guiActive = false)]
+        [KSPEvent(guiName = "Shutdown Heat Pump", guiActive = true)]
         public void Shutdown ()
         {
             isActive = false;
-            base.Events ["Shutdown"].active = false;
-            base.Events ["Activate"].active = true;
+            Events ["Shutdown"].active = false;
+            Events ["Activate"].active = true;
+            //Events ["Shutdown"].guiActive = false;
+            //Events ["Activate"].guiActive = true;
         }
 
         [KSPField(isPersistant = true)]
@@ -81,11 +87,15 @@ namespace RealFuels
 
         public List<ResourceRate> resources;
 
+        public List<AttachNode> attachNodes = new List<AttachNode>();
+        public List<string> attachNodeNames = new List<string>(); 
+
         public override string GetInfo ()
         {
             string s;
             s = "Heat Pump: " + heatTransfer + "/s\nRequirements:\n";
-            foreach (ResourceRate resource in resources) {
+            foreach (ResourceRate resource in resources)
+            {
                 if(resource.rate > 1)
                     s += "  " + resource.name + ": " + resource.rate.ToString ("2F") + "/s\n";
                 else if(resource.rate > 0.01666667f)
@@ -101,40 +111,90 @@ namespace RealFuels
         {
             base.OnAwake ();
             resources = new List<ResourceRate> ();
+            attachNodes = new List<AttachNode>();
             part.heatConductivity = heatConductivity;
             part.heatDissipation = heatDissipation;
+            print("*RF* ModuleHeatPump.Awake(): heatConductivity / heatDissipation = " + part.heatConductivity.ToString() + " / " + part.heatDissipation.ToString());
         }
 
         public override void OnLoad (ConfigNode node)
         {
             base.OnLoad (node);
-            foreach (ConfigNode n in node.GetNodes ("RESOURCE")) {
-                if(n.HasValue ("name") && n.HasValue ("rate")) {
+            foreach (ConfigNode n in node.GetNodes ("RESOURCE")) 
+            {
+                if(n.HasValue ("name") && n.HasValue ("rate")) 
+                {
                     float rate;
                     float.TryParse (n.GetValue ("rate"), out rate);
                     resources.Add (new ResourceRate(n.GetValue("name"), rate));
                 }
             }
+            foreach (ConfigNode c in node.GetNodes("HEATPUMP_NODE"))
+            {
+                // It would be easier to do this by just reading multiple names from one node
+                // Doing it this way allows for expansion later such as other attributes in each HEATPUMP_NODE
+                print("*RF* Heatpump searching HEATPUMP_NODE");
+                if (c.HasValue("name"))
+                {
+                    string nodeName = c.GetValue("name");
+                    print("*RF* Heatpump adding " + nodeName);
+                    attachNodeNames.Add(nodeName);
+                }
+
+            }
+
         }
 
         public override void OnStart (StartState state)
         {
             base.OnStart (state);
-            if(resources.Count == 0 && part.partInfo != null) {
+            part.heatConductivity = heatConductivity;
+            part.heatDissipation = heatDissipation;
+
+            Events ["Shutdown"].active = false;
+            Events ["Activate"].active = true;
+
+            if(resources.Count == 0 && part.partInfo != null) 
+            {
                 if(part.partInfo.partPrefab.Modules.Contains ("ModuleHeatPump"))
                     resources = ((ModuleHeatPump) part.partInfo.partPrefab.Modules["ModuleHeatPump"]).resources;
             }
+            if (attachNodes.Count == 0)
+            {
+                foreach (string nodeName in attachNodeNames)
+                {
+                    AttachNode node = this.part.findAttachNode(nodeName);
+                    if ((object)node != null)
+                        attachNodes.Add(node);
+                }
+            }
+            print("*RF* ModuleHeatPump.OnStart(): heatConductivity / heatDissipation = " + part.heatConductivity.ToString() + " / " + part.heatDissipation.ToString());
         }
 
         void FixedUpdate()
         {
-            if (HighLogic.LoadedSceneIsEditor || part.parent == null || !isActive)
+            if (!HighLogic.LoadedSceneIsFlight || !isActive)
+            {
                 return;
+            }
 
+            foreach (AttachNode attachNode in attachNodes)
+            {
+                Part targetPart = attachNode.attachedPart;
+
+                if ((object)targetPart == null)
+                    continue;
+
+                ProcessCooling(targetPart);
+            }
+            ProcessCooling(this.part.parent);
+
+            /*
             float efficiency = (part.parent.temperature + 273) / (part.parent.temperature + 300);
             if (part.parent.temperature < -273)
                 efficiency = 0;
-            if (heatTransfer < 0) {
+            if (heatTransfer < 0) 
+            {
                 efficiency = (part.temperature + 273) / (part.temperature + 300);
                 if(part.temperature < -273)
                     efficiency = 0;
@@ -148,6 +208,37 @@ namespace RealFuels
             }
             // this really should be linear, but KSP's current heat model is weird.
             part.parent.temperature -= efficiency * heatTransfer * Time.deltaTime / part.parent.mass;
+            part.temperature += efficiency * heatTransfer * heatGain * Time.deltaTime;
+            */
+        }
+
+        public void ProcessCooling(Part targetPart)
+        {
+            print("*RF* ModuleHeatPump processing part temperature and efficiency: " + targetPart.name);
+            float efficiency = (targetPart.temperature + 273) / (targetPart.temperature + 300);
+            if (targetPart.temperature < -273)
+                efficiency = 0;
+            if (heatTransfer < 0) 
+            {
+                efficiency = (part.temperature + 273) / (part.temperature + 300);
+                if(part.temperature < -273)
+                    efficiency = 0;
+            }
+            print("*RF* ModuleHeatPump processing resources");
+            foreach (ResourceRate resource in resources)
+            {
+                if(resource.rate > 0)
+                {
+                    float available = part.RequestResource(resource.id, resource.rate);
+                    if(efficiency > available / resource.rate)
+                        efficiency = available / resource.rate;
+                }
+            }
+            // this really should be linear, but KSP's current heat model is weird.
+            print("*RF* ModuleHeatPump setting targetPart temperature");
+            targetPart.temperature -= efficiency * heatTransfer * Time.deltaTime / targetPart.mass;
+            // target part ought to take into consideration resource mass too - Starwaster
+            print("*RF* ModuleHeatPump setting radiator temperature");
             part.temperature += efficiency * heatTransfer * heatGain * Time.deltaTime;
         }
     }
