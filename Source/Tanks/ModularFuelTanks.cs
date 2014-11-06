c2657122404aaa19a7ccf0b181b312998c84b77d
Bill Currie <bill@taniwha.org>
2014-11-06 19:52:24 +0900

    Break FuelInfo into its own file.

--- Source/Tanks/ModularFuelTanks.cs
+++ Source/Tanks/ModularFuelTanks.cs
@@ -28,12 +28,6 @@ namespace RealFuels.Tanks
             }
         }
 
-        // looks to see if we should ignore this fuel when creating an autofill for an engine
-        private static bool IgnoreFuel(string name)
-        {
-            return MFSSettings.Instance.ignoreFuelsForFill.Contains(name);
-        }
-
         private static MFSSettings Settings
         {
             get { return MFSSettings.Instance; }
@@ -1244,68 +1238,6 @@ namespace RealFuels.Tanks
             }
             if (activeChanged)
                 MarkWindowDirty();
-        }
-
-        private class FuelInfo
-        {
-            public string names;
-            public readonly List<Propellant> propellants;
-            public readonly double efficiency;
-            public readonly double ratioFactor;
-
-            public string Label
-            {
-                get
-                {
-                    string label = "";
-                    foreach (Propellant tfuel in propellants)
-                    {
-                        if (PartResourceLibrary.Instance.GetDefinition(tfuel.name).resourceTransferMode != ResourceTransferMode.NONE && !IgnoreFuel(tfuel.name))
-                        {
-                            if (label.Length > 0)
-                                label += " / ";
-                            label += Math.Round(100000 * tfuel.ratio / ratioFactor, 0)*0.001 + "% " + tfuel.name;
-                        }
-                    }
-                    return label;
-                }
-            }
-
-            public FuelInfo(List<Propellant> props, ModuleFuelTanks tank, string title)
-            {
-                // tank math:
-                // efficiency = sum[utilization * ratio]
-                // then final volume per fuel = fuel_ratio / fuel_utilization / efficiency
-
-                ratioFactor = 0.0;
-                efficiency = 0.0;
-                propellants = props;
-
-                foreach (Propellant tfuel in propellants)
-                {
-                    if (PartResourceLibrary.Instance.GetDefinition(tfuel.name) == null)
-                    {
-                        print("Unknown RESOURCE {" + tfuel.name + "}");
-                        ratioFactor = 0.0;
-                        break;
-                    }
-                    if (tank.dedicated || PartResourceLibrary.Instance.GetDefinition(tfuel.name).resourceTransferMode != ResourceTransferMode.NONE)
-                    {
-                        FuelTank t;
-                        if (tank.tankList.TryGet(tfuel.name, out t))
-                        {
-                            efficiency += tfuel.ratio/t.utilization;
-                            ratioFactor += tfuel.ratio;
-                        }
-                        else if (!IgnoreFuel(tfuel.name))
-                        {
-                            ratioFactor = 0.0;
-                            break;
-                        }
-                    }
-                }
-                names = "Used by: " + title;
-            }
         }
 
         public void ConfigureFor(Part engine)
