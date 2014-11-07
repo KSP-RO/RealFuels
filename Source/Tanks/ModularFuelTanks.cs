da6382088ed8b9bb1ff6ef89099b12e9cae31103
Bill Currie <bill@taniwha.org>
2014-11-08 08:16:23 +0900

    Put utilization back into the tank cost.
    I had a chance to talk with Nathan and it's intended that the cost is per
    tank volume, not contents volume.

--- Source/Tanks/ModularFuelTanks.cs
+++ Source/Tanks/ModularFuelTanks.cs
@@ -656,7 +656,7 @@ namespace RealFuels.Tanks
                         {
                             PartResourceDefinition d = PartResourceLibrary.Instance.GetDefinition(t.resource.resourceName);
                             if ((object)d != null)
-                                cst += t.maxAmount * (d.unitCost + t.cost);
+                                cst += t.maxAmount * (d.unitCost + t.cost / t.utilization);
                         }
                     }
                 }
