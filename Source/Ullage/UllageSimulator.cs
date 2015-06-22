e815a624dc8c8a3f0b99f371a79faf33337926e0
NathanKell <nathankell@gmail.com>
2015-06-22 01:43:57 -0400

    Speed up ullage sim, make params non-optional

--- Source/Ullage/UllageSimulator.cs
+++ Source/Ullage/UllageSimulator.cs
@@ -35,16 +35,16 @@ namespace RealFuels.Ullage
             ullageRadialMin = 0.0d; ullageRadialMax = 0.95d;
         }
 
-        public void Update(Vector3d localAcceleration, Vector3d rotation, double deltaTime, double ventingAcc = 0d, double fuelRatio = 0.9d)
+        public void Update(Vector3d localAcceleration, Vector3d rotation, double deltaTime, double ventingAcc, double fuelRatio)
         {
             double fuelRatioFactor = (0.5d + fuelRatio) * (1d/ 1.4d);
-            double invFuelRatioFactor = 1.0d / fuelRatioFactor;
+            double fuelRatioFactorRecip = 1.0d / fuelRatioFactor;
 
             //if (ventingAcc != 0.0f) Debug.Log("BoilOffAcc: " + ventingAcc.ToString("F8"));
             //else Debug.Log("BoilOffAcc: No boiloff.");
             
             Vector3d localAccelerationAmount = localAcceleration * deltaTime;
-            Vector3d rotationAmount = rotationAmount = rotation * deltaTime;
+            Vector3d rotationAmount = rotation * deltaTime;
 
             //Debug.Log("Ullage: dt: " + deltaTime.ToString("F2") + " localAcc: " + localAcceleration.ToString() + " rotateRate: " + rotation.ToString());
 
@@ -52,7 +52,7 @@ namespace RealFuels.Ullage
             //Debug.Log("Ullage: LocalAcc: " + localAcceleration.ToString());
             if (ventingAcc <= RFSettings.Instance.ventingAccThreshold)
             {
-                double ventingConst = (1d - ventingAcc / RFSettings.Instance.ventingAccThreshold) * invFuelRatioFactor * deltaTime;
+                double ventingConst = (1d - ventingAcc / RFSettings.Instance.ventingAccThreshold) * fuelRatioFactorRecip * deltaTime;
                 ullageHeightMin = UtilMath.Lerp(ullageHeightMin, 0.05d, RFSettings.Instance.naturalDiffusionRateY * ventingConst);
                 ullageHeightMax = UtilMath.Lerp(ullageHeightMax, 0.95d, RFSettings.Instance.naturalDiffusionRateY * ventingConst);
                 ullageRadialMin = UtilMath.Lerp(ullageRadialMin, 0.00d, RFSettings.Instance.naturalDiffusionRateX * ventingConst);
@@ -60,10 +60,11 @@ namespace RealFuels.Ullage
             }
 
             // Translate forward/backward.
+            double absAccY = Math.Abs(localAccelerationAmount.y);
             ullageHeightMin = UtilMath.Clamp(ullageHeightMin + localAccelerationAmount.y * RFSettings.Instance.translateAxialCoefficientY * fuelRatioFactor, 0.0d, 0.9d);
             ullageHeightMax = UtilMath.Clamp(ullageHeightMax + localAccelerationAmount.y * RFSettings.Instance.translateAxialCoefficientY * fuelRatioFactor, 0.1d, 1.0d);
-            ullageRadialMin = UtilMath.Clamp(ullageRadialMin - Math.Abs(localAccelerationAmount.y) * RFSettings.Instance.translateAxialCoefficientX * fuelRatioFactor, 0.0d, 0.9d);
-            ullageRadialMax = UtilMath.Clamp(ullageRadialMax + Math.Abs(localAccelerationAmount.y) * RFSettings.Instance.translateAxialCoefficientX * fuelRatioFactor, 0.1d, 1.0d);
+            ullageRadialMin = UtilMath.Clamp(ullageRadialMin - absAccY * RFSettings.Instance.translateAxialCoefficientX * fuelRatioFactor, 0.0d, 0.9d);
+            ullageRadialMax = UtilMath.Clamp(ullageRadialMax + absAccY * RFSettings.Instance.translateAxialCoefficientX * fuelRatioFactor, 0.1d, 1.0d);
 
             // Translate up/down/left/right.
             Vector3d sideAcc = new Vector3d(localAccelerationAmount.x, 0.0d, localAccelerationAmount.z);
@@ -88,24 +89,21 @@ namespace RealFuels.Ullage
             ullageRadialMax = UtilMath.Clamp(ullageRadialMax + rotateYawPitch.magnitude * RFSettings.Instance.rotateYawPitchCoefficientX, 0.1d, 1.0d);
 
             // Rotate roll.
-            ullageHeightMin = UtilMath.Clamp(ullageHeightMin - Math.Abs(rotation.y) * RFSettings.Instance.rotateRollCoefficientY * fuelRatioFactor, 0.0d, 0.9d);
-            ullageHeightMax = UtilMath.Clamp(ullageHeightMax + Math.Abs(rotation.y) * RFSettings.Instance.rotateRollCoefficientY * fuelRatioFactor, 0.1d, 1.0d);
-            ullageRadialMin = UtilMath.Clamp(ullageRadialMin - Math.Abs(rotation.y) * RFSettings.Instance.rotateRollCoefficientX * fuelRatioFactor, 0.0d, 0.9d);
-            ullageRadialMax = UtilMath.Clamp(ullageRadialMax - Math.Abs(rotation.y) * RFSettings.Instance.rotateRollCoefficientX * fuelRatioFactor, 0.1d, 1.0d);
+            double absRotY = Math.Abs(rotation.y);
+            ullageHeightMin = UtilMath.Clamp(ullageHeightMin - absRotY * RFSettings.Instance.rotateRollCoefficientY * fuelRatioFactor, 0.0d, 0.9d);
+            ullageHeightMax = UtilMath.Clamp(ullageHeightMax + absRotY * RFSettings.Instance.rotateRollCoefficientY * fuelRatioFactor, 0.1d, 1.0d);
+            ullageRadialMin = UtilMath.Clamp(ullageRadialMin - absRotY * RFSettings.Instance.rotateRollCoefficientX * fuelRatioFactor, 0.0d, 0.9d);
+            ullageRadialMax = UtilMath.Clamp(ullageRadialMax - absRotY * RFSettings.Instance.rotateRollCoefficientX * fuelRatioFactor, 0.1d, 1.0d);
 
             //Debug.Log("Ullage: Height: (" + ullageHeightMin.ToString("F2") + " - " + ullageHeightMax.ToString("F2") + ") Radius: (" + ullageRadialMin.ToString("F2") + " - " + ullageRadialMax.ToString("F2") + ")");
 
             double bLevel = UtilMath.Clamp((ullageHeightMax - ullageHeightMin) * (ullageRadialMax - ullageRadialMin) * 10d * UtilMath.Clamp(8.2d - 8d * fuelRatio, 0.0d, 8.2d) - 1.0d, 0.0d, 15.0d);
             //Debug.Log("Ullage: bLevel: " + bLevel.ToString("F3"));
 
-            double pVertical = 1.0d;
-            pVertical = 1.0d - (ullageHeightMin - 0.1d) / 0.2d;
-            pVertical = UtilMath.Clamp01(pVertical);
+            double pVertical = UtilMath.Clamp01(1.0d - (ullageHeightMin - 0.1d) * 5d);
             //Debug.Log("Ullage: pVertical: " + pVertical.ToString("F3"));
 
-            double pHorizontal = 1.0d;
-            pHorizontal = 1.0d - (ullageRadialMin - 0.1d) / 0.2d;
-            pHorizontal = UtilMath.Clamp01(pHorizontal);
+            double pHorizontal = UtilMath.Clamp01(1.0d - (ullageRadialMin - 0.1d) * 5d);
             //Debug.Log("Ullage: pHorizontal: " + pHorizontal.ToString("F3"));
 
             propellantStability = Math.Max(0.0d, 1.0d - (pVertical * pHorizontal * (0.75d + Math.Sqrt(bLevel))));
