efda459cccfccb544e617b0f0bbfd0a1c941f506
NathanKell <nathankell@gmail.com>
2017-08-06 02:37:42 -0700

    Min and max utilization support

--- Source/Tanks/ModuleFuelTanks.cs
+++ Source/Tanks/ModuleFuelTanks.cs
@@ -105,6 +105,11 @@ namespace RealFuels.Tanks
 				return;
 			}
 
+            UI_FloatRange f = (UI_FloatRange)(Fields["utilization"].uiControlEditor);
+            f.minValue = minUtilization;
+            f.maxValue = maxUtilization;
+            utilization = Mathf.Clamp(utilization, minUtilization, maxUtilization);
+
 			if (MFSSettings.tankDefinitions == null) {
 				MFSSettings.Initialize ();
 			}
@@ -481,6 +486,12 @@ namespace RealFuels.Tanks
 
 		[KSPField]
 		public bool utilizationTweakable = false;
+
+        [KSPField]
+        public float minUtilization = 1f;
+
+        [KSPField]
+        public float maxUtilization = 100f;
 
 		// no double support for KSPFields - [KSPField (isPersistant = true)]
 		public double volume;
