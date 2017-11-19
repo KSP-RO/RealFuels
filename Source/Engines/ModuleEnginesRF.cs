f9d1e46afdfb7a835b492bc13b5abf6680e43a53
Alex Wang <aw1621107@gmail.com>
2017-11-18 19:45:07 -0500

    Be a bit more explicit about whether ullage is required

--- Source/Engines/ModuleEnginesRF.cs
+++ Source/Engines/ModuleEnginesRF.cs
@@ -583,8 +583,7 @@ namespace RealFuels
                 output += "Pressure-fed";
             if (ignitions >= 0 && RFSettings.Instance.limitedIgnitions)
                 output += (output != string.Empty ? ", " : string.Empty) + "Ignitions: " + (ignitions > 0 ? ignitions.ToString() : "Ground only");
-            if (!ullage)
-                output += (output != string.Empty ? ", " : string.Empty) + "Not subject to ullage";
+            output += (output != string.Empty ? ", " : string.Empty) + (ullage ? "Subject" : "Not subject") + " to ullage";
             if (output != string.Empty)
                 output += "\n";
 
