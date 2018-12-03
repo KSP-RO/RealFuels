c4424ba69b60179472b21d7fa073794592acc875
Starwaster <starwaster@gmail.com>
2018-12-03 18:12:15 -0500

    Update ModuleFuelTanksRF.cs
    * MLI insulation is additive to any existing skin-internal conduction. (fixes potential issue of part defining lower skin-internal conductivity than MLI which would result in increase of overall conduction, even part internal-internal)

--- Source/Tanks/ModuleFuelTanksRF.cs
+++ Source/Tanks/ModuleFuelTanksRF.cs
@@ -129,7 +129,7 @@ namespace RealFuels.Tanks
             {
                 double normalizationFactor = 1 / (PhysicsGlobals.SkinInternalConductionFactor * PhysicsGlobals.ConductionFactor * PhysicsGlobals.ThermalConvergenceFactor * part.partInfo.partPrefab.skinInternalConductionMult * 10 * 0.5);
                 double insulationFactor = Math.Abs(GetMLITransferRate(part.skinTemperature, part.temperature) / (part.skinTemperature - part.temperature)) * 0.001;
-                part.heatConductivity = normalizationFactor * insulationFactor;
+                part.heatConductivity = normalizationFactor * 1/(1/insulationFactor + 1/part.partInfo.partPrefab.skinInternalConductionMult);
                 part.analyticInternalInsulationFactor = part.partInfo.partPrefab.analyticInternalInsulationFactor * (part.heatConductivity / part.partInfo.partPrefab.heatConductivity) * RFSettings.Instance.analyticInsulationMultiplier;
             }
 #if DEBUG
