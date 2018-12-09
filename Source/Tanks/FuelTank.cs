09a6d16b69a2c322366667cbe9b66cb047e6f7cc
Bill Currie <bill@taniwha.org>
2018-12-09 13:13:28 +0900

    Fix the "part already contains resource" messages.
    Should fix wonky delta-v indication when using LV-Ns.

--- Source/Tanks/FuelTank.cs
+++ Source/Tanks/FuelTank.cs
@@ -203,6 +203,7 @@ namespace RealFuels.Tanks
 			maxAmountExpression = null;
 
 			part.Resources.Remove (partResource);
+			part.SimulationResources.Remove (partResource);
 			module.RaiseResourceListChanged ();
 			//print ("Removed.");
 
@@ -211,6 +212,7 @@ namespace RealFuels.Tanks
 				foreach (Part sym in part.symmetryCounterparts) {
 					PartResource symResc = sym.Resources[name];
 					sym.Resources.Remove (symResc);
+					sym.SimulationResources.Remove (symResc);
 					RaiseResourceListChanged (sym);
 				}
 			}
