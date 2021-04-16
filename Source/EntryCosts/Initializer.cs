535d8c2e926ed06a07e83a6ad2a33bf5ed3208eb
siimav <siim.aaver@gmail.com>
2021-04-16 20:24:30 +0300

    Change the timing for part entry cost updating to fix bulk part purchases (#257)

--- Source/EntryCosts/Initializer.cs
+++ Source/EntryCosts/Initializer.cs
@@ -16,6 +16,16 @@ namespace RealFuels
             EntryCostDatabase.Initialize();
 
             EntryCostDatabase.UpdatePartEntryCosts();
+
+            // KSP's Funding binds to the same event and is used for deducting funds for entry purchases.
+            // RF will need to bind to the event before KSP itself does to process ECMs right after KSP has updated the funding.
+            // Note that KSP fires events in a reverse while loop.
+            GameEvents.OnPartPurchased.Add(OnPartPurchased);
+        }
+
+        private void OnPartPurchased(AvailablePart ap)
+        {
+            EntryCostManager.Instance?.OnPartPurchased(ap);
         }
     }
 }
