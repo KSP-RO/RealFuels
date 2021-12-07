489c05fd16d2d823540a797ce683af40aa65813e
NathanKell <nathankell@gmail.com>
2021-12-07 00:08:55 -0800

    Don't multiply fuel flow by 1000. @al2me6

--- Source/Engines/SolverRF.cs
+++ Source/Engines/SolverRF.cs
@@ -74,8 +74,8 @@ namespace RealFuels
             bool solid,
             int nSeed)
         {
-            minFlow = nMinFlow * 1000d; // to kg
-            maxFlow = nMaxFlow * 1000d;
+            minFlow = nMinFlow;
+            maxFlow = nMaxFlow;
             maxFlowRecip = 1d / maxFlow;
             atmosphereCurve = nAtmosphereCurve;
             atmCurve = nAtmCurve;
@@ -254,8 +254,7 @@ namespace RealFuels
                 // or fix fuel flow here in light of MR. But it's mostly just a visual bug, since the variation will be fine in most cases.
 
                 // apply fuel flow multiplier
-                double ffMult = fuelFlow * fuelFlowMult;
-                fuelFlow = ffMult;
+                fuelFlow *= fuelFlowMult;
 
                 double ispOtherMult = 1d;
                 if (atmCurveIsp != null)
@@ -275,7 +274,7 @@ namespace RealFuels
                 double exhaustVelocity = Isp * 9.80665d;
                 SFC = 3600d / Isp;
 
-                thrust = ffMult * exhaustVelocity; // either way, thrust is base * mult * EV
+                thrust = fuelFlow * exhaustVelocity * 1000d; // either way, thrust is base * mult * EV
 
                 // Calculate chamber temperature as ratio
                 double desiredTempRatio = Math.Max(tempMin, fxPower);
