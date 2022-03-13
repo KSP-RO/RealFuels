c4066b77a23812bd5296712794cb0b89c8c59ecb
DRVeyl <DRVeyl@gmail.com>
2022-03-12 22:53:50 -0500

    Consistent FuelTank.amount change handling
    Extract updating maxAmount and amount into a method, to consistently fire events and update the PAW resource items.
    Remove unused/depreated features
    Tabify

--- Source/Tanks/FuelTank.cs
+++ Source/Tanks/FuelTank.cs
@@ -6,48 +6,48 @@ using UnityEngine;
 
 namespace RealFuels.Tanks
 {
-    // A FuelTank is a single TANK {} entry from the part.cfg file.
-    // it defines four properties:
-    // name         The name of the resource that can be stored.
-    // utilization  How much of the tank is devoted to that resource (vs.
-    //              how much is wasted in cryogenics or pumps).
-    //              This is in resource units per volume unit.
-    // mass         How much the part's mass is increased per volume unit
-    //              of tank installed for this resource type. Tons per
-    //              volume unit.
-    // temperature  the part temperature at which this tank's contents start boiling
-    // loss_rate    How quickly this resource type bleeds out of the tank. 
-    //              (TODO: instead of this unrealistic static loss_rate, all 
-    //              resources should have vsp (heat of vaporization) added and optionally conduction)
-    //
+	// A FuelTank is a single TANK {} entry from the part.cfg file.
+	// it defines four properties:
+	// name         The name of the resource that can be stored.
+	// utilization  How much of the tank is devoted to that resource (vs.
+	//              how much is wasted in cryogenics or pumps).
+	//              This is in resource units per volume unit.
+	// mass         How much the part's mass is increased per volume unit
+	//              of tank installed for this resource type. Tons per
+	//              volume unit.
+	// temperature  the part temperature at which this tank's contents start boiling
+	// loss_rate    How quickly this resource type bleeds out of the tank. 
+	//              (TODO: instead of this unrealistic static loss_rate, all 
+	//              resources should have vsp (heat of vaporization) added and optionally conduction)
+	//
 
-    public class FuelTank: IConfigNode
+	public class FuelTank : IConfigNode
 	{
 		//------------------- fields
 		[Persistent]
 		public string name = "UnknownFuel";
 		[Persistent]
 		public string note = "";
-      
-        public string boiloffProduct = "";
 
-        [Persistent]
+		public string boiloffProduct = "";
+
+		[Persistent]
 		public float utilization = 1.0f;
 		[Persistent]
 		public float mass = 0.0f;
 		[Persistent]
 		public float cost = 0.0f;
-        // TODO Retaining for fallback purposes but should be deprecated
+		// TODO Retaining for fallback purposes but should be deprecated
 		[Persistent]
 		public double loss_rate = 0.0;
 
-        public double vsp;
+		public double vsp;
 
-        public double resourceConductivity = 10;
+		public double resourceConductivity = 10;
 
-        // cache for tank.totalArea and tank.tankRatio for use by ModuleFuelTanksRF
-        public double totalArea = -1;
-        public double tankRatio = -1;
+		// cache for tank.totalArea and tank.tankRatio for use by ModuleFuelTanksRF
+		public double totalArea = -1;
+		public double tankRatio = -1;
 
 		[Persistent]
 		public double wallThickness = 0.1;
@@ -67,8 +67,6 @@ namespace RealFuels.Tanks
 		[Persistent]
 		public string techRequired = "";
 
-		public bool locked = false;
-
 		public bool propagate = true;
 
 		public double density = 0d;
@@ -81,46 +79,47 @@ namespace RealFuels.Tanks
 		[NonSerialized]
 		private ModuleFuelTanks module;
 
-
-        public PartResourceDefinition boiloffProductResource;
+		public PartResourceDefinition boiloffProductResource;
 
 		//------------------- virtual properties
 		public Part part => module != null ? module.part : null;
 
 		public PartResource resource => part != null ? part.Resources[name] : null;
 
-		public void RaiseResourceInitialChanged (Part part, PartResource resource, double amount)
+		public void RaiseResourceInitialChanged(Part part, PartResource resource, double amount)
 		{
-			var data = new BaseEventDetails (BaseEventDetails.Sender.USER);
-			data.Set<PartResource> ("resource", resource);
-			data.Set<double> ("amount", amount);
-			part.SendEvent ("OnResourceInitialChanged", data, 0);
+			var data = new BaseEventDetails(BaseEventDetails.Sender.USER);
+			data.Set<PartResource>("resource", resource);
+			data.Set<double>("amount", amount);
+			part.SendEvent("OnResourceInitialChanged", data, 0);
 		}
 
-		public void RaiseResourceMaxChanged (Part part, PartResource resource, double amount)
+		public void RaiseResourceMaxChanged(Part part, PartResource resource, double amount)
 		{
-			var data = new BaseEventDetails (BaseEventDetails.Sender.USER);
-			data.Set<PartResource> ("resource", resource);
-			data.Set<double> ("amount", amount);
-			part.SendEvent ("OnResourceMaxChanged", data, 0);
+			var data = new BaseEventDetails(BaseEventDetails.Sender.USER);
+			data.Set<PartResource>("resource", resource);
+			data.Set<double>("amount", amount);
+			part.SendEvent("OnResourceMaxChanged", data, 0);
 		}
 
-		public void RaiseResourceListChanged (Part part)
+		public void RaiseResourceListChanged(Part part)
 		{
-			part.ResetSimulationResources ();
-			part.SendEvent ("OnResourceListChanged", null, 0);
+			part.ResetSimulationResources();
+			part.SendEvent("OnResourceListChanged", null, 0);
 		}
 
 		public double amount
 		{
-			get {
+			get
+			{
 				if (module == null)
-					throw new InvalidOperationException ("Amount is not defined until instantiated in a tank");
+					throw new InvalidOperationException("Amount is not defined until instantiated in a tank");
 				return (resource != null) ? resource.amount : 0;
 			}
-			set {
+			set
+			{
 				if (module == null)
-					throw new InvalidOperationException ("Amount is not defined until instantiated in a tank");
+					throw new InvalidOperationException("Amount is not defined until instantiated in a tank");
 
 				PartResource partResource = resource;
 				// Point of unmanaged resource is we don't manage them.  So bail out here.
@@ -135,18 +134,10 @@ namespace RealFuels.Tanks
 
 				amountExpression = null;
 
-				partResource.amount = value;
-				if (HighLogic.LoadedSceneIsEditor) {
-					module.RaiseResourceInitialChanged(partResource, value);
-					if (propagate) {
-						foreach (Part sym in part.symmetryCounterparts)
-						{
-							PartResource symResc = sym.Resources[name];
-							symResc.amount = value;
-							RaiseResourceInitialChanged(sym, symResc, value);
-						}
-					}
-				}
+				UpdateAmount(partResource, value);
+				if (HighLogic.LoadedSceneIsEditor && propagate)
+					foreach (Part sym in part.symmetryCounterparts)
+						UpdateAmount(sym.Resources[name], value);
 			}
 		}
 
@@ -178,7 +169,7 @@ namespace RealFuels.Tanks
 			}
 		}
 
-		void UpdateTank (double value)
+		void UpdateTank(double value)
 		{
 			PartResource partResource = resource;
 			if (module.unmanagedResources.ContainsKey(partResource.resourceName))
@@ -195,23 +186,12 @@ namespace RealFuels.Tanks
 			if (value == partResource.maxAmount)
 				return;
 
-			double fillFrac = fillFraction;	// fillFraction is a live value, gather it before changing a resource amount
+			double fillFrac = FillFraction; // fillFraction is a live value, gather it before changing a resource amount
+			double newAmount = value * fillFrac;    // Keep the same fill fraction
 			maxAmountExpression = null;
-			partResource.maxAmount = value;
-			module.RaiseResourceMaxChanged(partResource, value);
 
-			// Keep the same fill fraction
-			double newAmount = value * fillFrac;
-			if (newAmount != partResource.amount)
-			{
-				partResource.amount = newAmount;
-				module.RaiseResourceInitialChanged(partResource, newAmount);
-			}
-			if (partResource.part.PartActionWindow?.ListItems.FirstOrDefault(r => r is UIPartActionResourceEditor e && partResource == e.Resource) is UIPartActionResourceEditor resItem && resItem != null)
-			{
-				resItem.resourceAmnt.text = KSPUtil.LocalizeNumber(partResource.amount, "F1");
-				resItem.resourceMax.text = KSPUtil.LocalizeNumber(partResource.maxAmount, "F1");
-			}
+			UpdateMaxAmount(partResource, value);
+			UpdateAmount(partResource, newAmount);
 
 			// Update symmetry counterparts.
 			if (HighLogic.LoadedSceneIsEditor && propagate)
@@ -219,21 +199,29 @@ namespace RealFuels.Tanks
 				foreach (Part sym in part.symmetryCounterparts)
 				{
 					PartResource symResc = sym.Resources[name];
-					symResc.maxAmount = value;
-					RaiseResourceMaxChanged(sym, symResc, value);
-
-					if (newAmount != symResc.amount)
-					{
-						symResc.amount = newAmount;
-						RaiseResourceInitialChanged(sym, symResc, newAmount);
-					}
-					if (symResc.part.PartActionWindow?.ListItems.FirstOrDefault(r => r is UIPartActionResourceEditor e && symResc == e.Resource) is UIPartActionResourceEditor symResItem && symResItem != null)
-					{
-						symResItem.resourceAmnt.text = KSPUtil.LocalizeNumber(symResc.amount, "F1");
-						symResItem.resourceMax.text = KSPUtil.LocalizeNumber(symResc.maxAmount, "F1");
-					}
+					UpdateMaxAmount(symResc, value);
+					UpdateAmount(symResc, newAmount);
 				}
 			}
+		}
+
+		private void UpdateAmount(PartResource partResource, double newAmount)
+		{
+			if (newAmount != partResource.amount)
+			{
+				partResource.amount = newAmount;
+				RaiseResourceInitialChanged(partResource.part, partResource, newAmount);
+			}
+			if (partResource.part.PartActionWindow?.ListItems.FirstOrDefault(r => r is UIPartActionResourceEditor e && partResource == e.Resource) is UIPartActionResourceEditor resItem && resItem != null)
+				resItem.resourceAmnt.text = KSPUtil.LocalizeNumber(newAmount, "F1");
+		}
+
+		private void UpdateMaxAmount(PartResource partResource, double newAmount)
+		{
+			partResource.maxAmount = newAmount;
+			RaiseResourceMaxChanged(partResource.part, partResource, newAmount);
+			if (partResource.part.PartActionWindow?.ListItems.FirstOrDefault(r => r is UIPartActionResourceEditor e && partResource == e.Resource) is UIPartActionResourceEditor resItem && resItem != null)
+				resItem.resourceMax.text = KSPUtil.LocalizeNumber(newAmount, "F1");
 		}
 
 		void AddTank(double value)
@@ -257,31 +245,31 @@ namespace RealFuels.Tanks
 
 			module.RaiseResourceListChanged();
 
-            // Update symmetry counterparts.
+			// Update symmetry counterparts.
 			if (HighLogic.LoadedSceneIsEditor && propagate)
-            {
+			{
 				foreach (Part sym in part.symmetryCounterparts)
-                {
-                    sym.Resources.dict.Add(resDef.id, new PartResource(res));
-                    RaiseResourceListChanged(sym);
-                }
-            }
+				{
+					sym.Resources.dict.Add(resDef.id, new PartResource(res));
+					RaiseResourceListChanged(sym);
+				}
+			}
 		}
 
-		public double maxAmount {
-			get {
-				if (module == null) {
-					throw new InvalidOperationException ("Maxamount is not defined until instantiated in a tank");
-				}
+		public double maxAmount
+		{
+			get
+			{
+				if (module == null)
+					throw new InvalidOperationException("Maxamount is not defined until instantiated in a tank");
 				PartResource res = resource;
 				return (res == null || module.unmanagedResources.ContainsKey(res.resourceName)) ? 0 : res.maxAmount;
 			}
 
-			set {
-				if (module == null) {
-					throw new InvalidOperationException ("Maxamount is not defined until instantiated in a tank");
-				}
-				//print ("*RK* Setting maxAmount of tank " + name + " of part " + part.name + " to " + value);
+			set
+			{
+				if (module == null)
+					throw new InvalidOperationException("Maxamount is not defined until instantiated in a tank");
 
 				PartResource partResource = resource;
 				if (partResource != null && value <= 0.0)
@@ -294,7 +282,7 @@ namespace RealFuels.Tanks
 			}
 		}
 
-		public double fillFraction
+		public double FillFraction
 		{
 			get => amount / maxAmount;
 			set => amount = value * maxAmount;
@@ -303,33 +291,30 @@ namespace RealFuels.Tanks
 		public override string ToString() => name ?? "NULL";
 
 		//------------------- IConfigNode implementation
-		public void Load (ConfigNode node)
+		public void Load(ConfigNode node)
 		{
 			if (!(node.name.Equals("TANK") && node.HasValue("name")))
 				return;
 
 			ConfigNode.LoadObjectFromConfig(this, node);
-			if (node.HasValue("efficiency") && !node.HasValue("utilization"))
-				node.TryGetValue("efficiency", ref utilization);
-
 			node.TryGetValue("amount", ref amountExpression);
 			node.TryGetValue("maxAmount", ref maxAmountExpression);
 
 			resourceAvailable = PartResourceLibrary.Instance.GetDefinition(name) != null;
-            MFSSettings.resourceVsps.TryGetValue(name, out vsp);
-            MFSSettings.resourceConductivities.TryGetValue(name, out resourceConductivity);
+			MFSSettings.resourceVsps.TryGetValue(name, out vsp);
+			MFSSettings.resourceConductivities.TryGetValue(name, out resourceConductivity);
 
 			string boiloffRes = "";
 			if (node.TryGetValue("boiloffProduct", ref boiloffRes))
 				boiloffProductResource = PartResourceLibrary.Instance.GetDefinition(boiloffRes);
 
-            GetDensity();
+			GetDensity();
 		}
 
 		public void Save(ConfigNode node)
 		{
 			if (name == null) return;
-			ConfigNode.CreateConfigFromObject (this, node);
+			ConfigNode.CreateConfigFromObject(this, node);
 			node.AddValue("amount", module == null ? amountExpression : amount.ToString("G17"));
 			node.AddValue("maxAmount", module == null ? maxAmountExpression : maxAmount.ToString("G17"));
 		}
@@ -359,7 +344,8 @@ namespace RealFuels.Tanks
 			}
 			maxAmountExpression = null;
 
-			if (amountExpression == null) {
+			if (amountExpression == null)
+			{
 				amount = maxAmount;
 				return;
 			}
@@ -379,32 +365,32 @@ namespace RealFuels.Tanks
 		}
 
 		//------------------- Constructor
-		public FuelTank (ConfigNode node)
+		public FuelTank(ConfigNode node)
 		{
-			Load (node);
+			Load(node);
 		}
 
-		internal FuelTank CreateCopy (ModuleFuelTanks toModule, ConfigNode overNode, bool initializeAmounts)
+		internal FuelTank CreateCopy(ModuleFuelTanks toModule, ConfigNode overNode, bool initializeAmounts)
 		{
-			FuelTank clone = (FuelTank)MemberwiseClone ();
+			FuelTank clone = (FuelTank)MemberwiseClone();
 			clone.module = toModule;
 
-			if (overNode != null) {
-				clone.Load (overNode);
-			}
-			if (initializeAmounts) {
-				clone.InitializeAmounts ();
-			} else {
+			if (overNode != null)
+				clone.Load(overNode);
+
+			if (initializeAmounts)
+				clone.InitializeAmounts();
+			else
 				clone.amountExpression = clone.maxAmountExpression = null;
-			}
-            clone.GetDensity();
+
+			clone.GetDensity();
 			return clone;
 		}
 
-        internal void GetDensity()
-        {
-            PartResourceDefinition d = PartResourceLibrary.Instance.GetDefinition(name);
+		internal void GetDensity()
+		{
+			PartResourceDefinition d = PartResourceLibrary.Instance.GetDefinition(name);
 			density = (d != null) ? d.density : 0;
-        }
+		}
 	}
 }
