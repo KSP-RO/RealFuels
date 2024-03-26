f86c443161e6f3f052b1822325743060f4917086
Stonesmile <62522391+StonesmileGit@users.noreply.github.com>
2024-03-26 17:30:57 +0100

    Move harmony patch to separate project

---
+++ Source/B9PSPatch/B9PS.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System.Linq;
+using System.Collections.Generic;
+using B9PartSwitch.PartSwitch.PartModifiers;
+using B9PartSwitch;
+
+namespace RealFuels.Harmony
+{
+    [HarmonyPatch(typeof(ModuleModifierInfo))]
+    internal class PatchModuleModifierInfo
+    {
+        internal static bool Prepare()
+        {
+            bool foundB9PS = AssemblyLoader.loadedAssemblies.FirstOrDefault(a => a.name.Equals("B9PartSwitch", System.StringComparison.OrdinalIgnoreCase)) != null;
+            return foundB9PS;
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch("CreatePartModifiers")]
+        internal static IEnumerable<IPartModifier> Postfix_CreatePartModifiers(IEnumerable<IPartModifier> result, Part part, ModuleModifierInfo __instance, BaseEventDetails moduleDataChangedEventDetails)
+        {
+            foreach (var partModifier in result)
+            {
+                if (partModifier is ModuleDataHandlerBasic)
+                {
+                    ModuleMatcher moduleMatcher = new ModuleMatcher(__instance.identifierNode);
+                    PartModule module = moduleMatcher.FindModule(part);
+                    ConfigNode originalNode = moduleMatcher.FindPrefabNode(module);
+                    if (module.moduleName == "ModuleFuelTanks")
+                    {
+                        yield return new ModuleFuelTanksHandler(module, originalNode, __instance.dataNode, moduleDataChangedEventDetails);
+                        continue;
+                    }
+                }
+                yield return partModifier;
+            }
+
+        }
+    }
+}
---
+++ Source/B9PSPatch/assembly/AssemblyInfoB9PSPatch.cs
@@ -0,0 +1,36 @@
+#define CIBUILD_disabled
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("RealFuelsB9PSPatch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("KSP-RO Group")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.0.0")]
+// [assembly: AssemblyInformationalVersionAttribute("@FULL_VERSION@")]
+#if CIBUILD
+[assembly: AssemblyFileVersion("@MAJOR@.@MINOR@.@PATCH@.@BUILD@")]
+[assembly: KSPAssembly("RealFuelsB9PSPatch", @MAJOR@, @MINOR@, @PATCH@)]
+#else
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: KSPAssembly("RealFuelsB9PSPatch", 1, 0, 0)]
+#endif
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+[assembly: KSPAssemblyDependency("B9PartSwitch", 2, 20)]
